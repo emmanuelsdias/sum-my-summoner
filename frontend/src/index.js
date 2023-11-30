0c0bc6086fd70e936305f3eccb43a48330847a47
Emmanuel <emanusdias@gmail.com>
2023-11-30 13:11:40 -0300

    added helmet to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 import store from "./store";
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider context={helmetContext}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
