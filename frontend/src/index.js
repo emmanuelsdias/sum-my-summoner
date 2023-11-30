aa9cd0b65f5c4d76126e6bb76359cabac5805a70
Emmanuel <emanusdias@gmail.com>
2023-11-30 13:56:20 -0300

    fix helmet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <HelmetProvider context={helmetContext}>
+    <HelmetProvider context={{}}>
       <Provider store={store}>
         <App />
       </Provider>
